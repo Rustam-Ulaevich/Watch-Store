e4b56389eda4b9cc2a4a013b8947538110456407
Rustam-Ulaevich <praporglamur@gmail.com>
2024-01-25 09:32:13 +0500

    create new function withMap

--- js/script.js
+++ js/script.js
@@ -5,9 +5,23 @@ let items = [
     { price: 57},
 ]
 
-const calculateTotal = (goods) => {
-    
+ const calculateTotal = (goods) => {
+    let res = 0
+    for ( let i of goods){
+        if( typeof i.price === 'number' && i.price >= 0){
+            res += i.price
+        }
+    }
+    return res
 }
+
+const withMap = (arr) => {
+    let newArr = arr.map( el => +el.price)
+    return newArr.reduce((acm, val) => acm + val)
+}
+
+console.log(calculateTotal(items));
+console.log(withMap(items));
 
 
 
